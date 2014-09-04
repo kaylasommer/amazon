d8e1aa08a08945756714ec8b638b487eb28e9ae7
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-04 10:07:02 -0500

    products database

---
+++ app/models/products.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function Product(){
+}
+
+Object.defineProperty(Product, 'collection', {
+  get: function(){return global.mongodb.collection('products');}
+});
+
+Product.all = function(cb){
+  Product.collection.find().toArray(cb);
+};
+
+module.exports = Product;
